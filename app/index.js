07545de5000446406e957c4b4c7abd2bce655b88
Dave Kanter <moatgoat@hotmail.com>
2018-03-01 14:48:27 -0800

    Ready to add react.

--- app/index.js
+++ app/index.js
@@ -10,12 +10,16 @@ import * as json_manip from "./js/json_manip.js";
 const jsonData = require('./json/data.json');
 //console.log(jsonPath);
 
-json_manip.parseJSON(jsonData);
-
+let dataVar = json_manip.parseJSON(jsonData);
+console.log("DataVar: " + dataVar)
 
 //start
 console.log("Hello Williams-Sonoma!");
 
 
+//ARROW FUNCTIONS!
+let peg = () => {
+	console.log("Yup!");
+};
 
-
+peg();
